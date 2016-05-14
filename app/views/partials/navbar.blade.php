fbf54ebfd8fd00302b91f4e05607dbb88a85c739
Gaston Lenotre <gastonlenotre@gmail.com>
2016-05-13 22:37:23 -0500

    add talkingtreefarm.com to portfolio

--- app/views/partials/navbar.blade.php
+++ app/views/partials/navbar.blade.php
@@ -1,15 +1,14 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
-    <a href="/"><img src="/img/Gaston_navy-blue_circle.png" class="img-responsive center-block" alt="Gaston Logo" width="150" height="150"></a>
+  	<div class="col-sm-4"></div>
+    <div class="col-sm-4">
+    	<a href="/"><img src="/img/Gaston_navy-blue_circle.png" class="img-responsive center-block" alt="Gaston Logo" width="150" height="150"></a>
+    </div>
+    <div class="col-sm-4"></div>
     <ul class="nav navbar-nav">
-      <li><a href="/resume">Resume</a></li>
-      <li><a href="/portfolio">Portfolio</a></li>
-      <li><a href="/blog">Blog</a></li>
-      
-      {{-- {{ Form::open(['method'=>'GET', 'action'=>'PostsController@index', 'class'=>'navbar-form navbar-right','role'=>'search'])  }} --}}
-            {{-- <input type="text" name="search" class="form-control" placeholder="Search titles"> --}}
-      {{-- {{ Form::close() }} --}}
-
+      <li><a href="/resume"><b>Resume</b></a></li>
+      <li><a href="/portfolio"><b>Portfolio</b></a></li>
+      <li><a href="/blog"><b>Blog</b></a></li>
     </ul>
   </div> <!--  /.container-fluid  -->
 </nav>
--- app/views/portfolio.blade.php
+++ app/views/portfolio.blade.php
@@ -9,22 +9,57 @@
 
 @section('content')
 
-<section class="extra">
-
-	{{-- <div class="projects">
-	  	<ul>
-	  		<li class="project-screenshot p1">
-	  			<div class="project">
-	  				<h3><a target="_blank" class="foliolink" href="http://talkingtreefarm.com">Talking Tree Farm →</a></h3>
-	  				<p>Full site</p>
-	  			</div>
-	  			<a class="foliohover" href="#"><img width="400" height="400" src="/projects/annie_logo.png" class="attachment-post-thumbnail" alt=""></a>
-	  		</li>
-
-	  	</ul>
-	</div> --}}
-
-</section>
+	<div class="well">
+      	<div class="row">
+    		<div class="col-sm-3">
+	     		<div class = "text-center"><a href="http://talkingtreefarm.com" target="_blank">
+	    			<img class="img-responsive projects thumbnail" src="/img/rsz_talking_tree_farm.jpg"></a>
+	      			<div class="caption">
+			        <h3>Talking Tree Farm</h3>
+			        <p>eCommerce site for a local organic farm.</p>
+	       		</div>
+	      	</div>
+	    </div>
+    		<div class="col-sm-3">
+      			<div class = "text-center"><a href="http://s.codepen.io/glenotre/debug/RWdxqq" target="_blank">
+      				<img class="img-responsive projects thumbnail" src="/img/rsz_pomodoro_timer.jpg"></a>
+      				<div class="caption">
+        			<h3>Pomodoro Timer</h3>
+        			<p>Start a 25 minute pomodoro, and the timer will go off once 25 minutes has elapsed.</p>
+      			</div>
+      		</div>
+    	</div>
+	    	<div class="col-sm-3">
+	    		<div class = "text-center">
+			      <a href="http://s.codepen.io/glenotre/debug/epQGBx" target="_blank">
+			      	<img class="img-responsive projects thumbnail" src="/img/rsz_random_quote__machine.jpg"></a>
+			    	<div class="caption">
+				        <h3>Random GK Quote Machine</h3>
+				        <p>Find a random quote by G. K. Chesterton.</p>
+	      			</div>
+	      		</div>
+	    	</div>
+	    <div class="col-sm-3">
+		    <div class = "text-center">
+		     	<a href="http://s.codepen.io/glenotre/debug/EVMQBr" target="_blank">
+		    	<img class="img-responsive projects thumbnail" src= "/img/rsz_js_calculator.jpg"></a>
+			      	<div class="caption">
+			        	<h3>JS Calculator</h3>
+			        	<p>Chain mathematical operations together until you hit the equal button, and the calculator will tell you the correct output.</p>
+			      	</div>
+		    </div>
+	    </div>
+	    {{-- <div class="col-sm-3">
+	     	<div class = "text-center">
+	    		<img class="img-responsive projects thumbnail" src="">
+	      		<div class="caption">
+			        <h3>Weather Map API</h3>
+			        <p>See the weather in any location. Toggle between Fahrenheit and Celsius.</p>
+	       		</div>
+	      	</div>
+	    </div> --}}
+	  </div>
+	</div>
 
 @stop
 
